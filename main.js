54e62bf37e5a3066cade7f517d9fc6db66e9f4fb
Annie Huang <annie.hn@gmail.com>
2024-06-15 14:00:52 +1000

    Get the offset value of the mouse hover point from the center of the button

--- main.js
+++ main.js
@@ -5,5 +5,11 @@ glassyButtons.forEach((button) => {
     // Find the center point of the button.
     const centerX = button.offsetWidth / 2;
     const centerY = button.offsetHeight / 2;
+
+    // event.offset is the coordinate of the mouse on the button, start from top left corner as 0 0, and bottom right corner has the highest positive number
+    const offsetX = event.offsetX - centerX;
+    const offsetY = event.offsetY - centerY;
+    // Then offsetX and offsetY will be the different from the center of the the button.
+    // if we are right in the middle, offsetX offsetY will be 0 0. On the top left corner, it will have the most negative numbers and the bottom right corner has the most positive numbers
   });
 });
